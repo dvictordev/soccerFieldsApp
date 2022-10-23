d950194a3f9bb09fe1314b3aebaeae4d837c2e17
VictorDuarte <victordc133@gmail.com>
2022-10-23 09:46:16 -0300

    first commit

---
+++ src/theme/theme.ts
@@ -0,0 +1,12 @@
+export const THEME = {
+    colors:{
+        PRIMARY:'#0FBA00', 
+        SECONDARY:"#2F4E24",
+        TEXT_PRIMARY:"#fff",
+    },
+    fonts:{
+        SM:16,
+        MD:18,
+        LG:20
+    }
+}
